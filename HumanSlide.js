04e594fd38de10a9451094e2e330efbe42e7f836
pconin <pconin@gmail.com>
2018-04-26 19:27:08 +0200

    Multi image split ok

--- HumanSlide.js
+++ HumanSlide.js
@@ -3,7 +3,6 @@
 // https://stackoverflow.com/questions/21933043/split-an-image-using-javascript
 var def = {
 	div			: 'HumanSlide',
-	canvas		: 'HumanSlideCanvas',
 	activeImage : 0,
 	splitsNb	: 3
 }
@@ -18,8 +17,13 @@ class HumanSlide {
 		this.options	= options;
 		console.log(images);
 		console.log(options);
+		this.createStyle();
 		this.assertParams();
 		this.insertSplitImg();
+	}
+
+	createStyle(){
+
 	}
 
 	assertParams(){
@@ -28,23 +32,50 @@ class HumanSlide {
 		this.splitsNb		= this.options.splitsNb		|| def.splitsNb;
 
 		for(var i = 0; i < this.images.length; i++){
-			this.images[i] = {src : this.images[i], div : null, parts : []};
+			this.images[i] = {src : this.images[i], parts : []};
 		}
 		this.length = this.images.length;
 	}
 
 	insertSplitImg(){
-		for(var i = 0; i < this.images.length; i++){
-			console.log('insert split img');
-			// Insert image in div
-			var image = this.images[i];
-			var newImage = document.createElement("img");
-			//	newImage.style.display = 'none';
-				newImage.setAttribute("src", image.src);
-			this.div.appendChild(newImage);
-			image.div = newImage;
+		var self = this;
+		// Create image and get ratio
+		var image = document.createElement('img');
+		image.id  = 'image';
+		image.src = this.images[0].src;
+		this.div.appendChild(image);
 
-			// Split image and assert
+		image.onload = function () {
+			var ratio = this.height / this.width;
+
+			console.log(ratio);
+			this.style.display = 'none';
+			self.div.style.height = ratio * self.div.offsetWidth + 'px';
+
+			for(var i = 0; i < self.images.length; i++){
+				console.log('insert split img');
+				// Create a div
+				var image = self.images[i];
+				// Add classes puzzle and id piece + i
+				for(var j = 0; j < self.splitsNb; j++){
+					var yPercent = (100 / (self.splitsNb - 1)) * j;
+
+					console.log(self.splitsNb , j);
+					var part		= document.createElement('div');
+					part.className += 'puzzle';
+					part.id			= 'piece' + i + 'part' + j;
+					part.setAttribute('style', 'background-image:url(\''+ image.src +'\');background-position:0% ' + yPercent + '%;');
+					console.log(part);
+					self.div.appendChild(part);
+
+					image.parts[j]	= part;
+				}
+
+
+
+			}
 		}
+
+
 	}
 }
